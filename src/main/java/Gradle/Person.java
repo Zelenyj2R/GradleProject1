a0fe6676cc383ae16362b1b009e492c8ca970ef3
Artem Knyzhnykov <111915140+Zelenyj2R@users.noreply.github.com>
2023-03-28 16:09:23 +0200

    Initial commit

---
+++ src/main/java/Gradle/Person.java
@@ -0,0 +1,19 @@
+package Gradle;
+
+public class Person {
+    private String name = "Artem";
+    private String lastName = "Knyznykov";
+
+    public Person() {
+        this.name = name;
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return name;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+}
